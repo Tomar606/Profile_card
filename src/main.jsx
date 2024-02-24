d7a2bb12ae51f1268823c384c8680edc6e3a55b0
Tomar606 <itsgaurav606@gmail.com>
2024-02-24 18:38:58 +0530

    error rem

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import UserProfileCard from './components/UserProfileCard/UserProfileCard.jsx'
+import UserProfileCard from './UserProfileCard.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
